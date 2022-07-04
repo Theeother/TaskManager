5d0213b253668628787bebaf0fb50de16245a954
AKS <ka.seddik@gmail.com>
2022-07-04 13:06:39 +0000

    removing redundant code from TaskForm

--- todo/src/components/TaskForm.js
+++ todo/src/components/TaskForm.js
@@ -7,6 +7,7 @@ function TaskForm({
   const [ description, setDescription ] = useState(editing ? task.description : '');
   const [ priority, setPriority ] = useState(editing ? task.priority : 0);
   const [ color, setColor ] = useState(editing ? task.color : 'primary');
+  const [ done ] = useState(editing ? task.done : false);
 
   const saveNewTask = (e) => {
     e.preventDefault();
@@ -45,44 +46,78 @@ function TaskForm({
 
   return (
     <div>
-      {!editing ? (
-        <form onSubmit={(e) => saveNewTask(e)}>
-          <label
-            htmlFor="title"
-            className="form-label"
-          >
-            Title
-          </label>
-          <input
-            type="text"
-            id="title"
-            className="form-control"
-            value={title}
-            placeholder="New task title"
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-
-          <label
-            htmlFor="desc"
-            className="form-label"
-          >
-            Description
-          </label>
-          <input
-            type="text"
-            id="desc"
-            className="form-control"
-            value={description}
-            placeholder="New task description"
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          />
-
-          <label htmlFor="color" className="form-label">Color</label>
+      <div className={`mx-5 card text-center ${done ? 'border-' : 'text-bg-'}${editing ? color : 'light'}`}>
+        <form onSubmit={(e) => {
+          if (editing) EditTask(e);
+          else {
+            saveNewTask(e);
+          }
+        }}
+        >
+          <div className="card-header px-5">
+            {!editing && (
+              <label
+                htmlFor="title"
+                className="form-label"
+              >
+                Title
+              </label>
+            )}
+            <input
+              type="text"
+              id="title"
+              className="form-control"
+              value={title}
+              placeholder="New task title"
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </div>
+          <div className="card-body">
+            {!editing && (
+              <label
+                htmlFor="desc"
+                className="form-label"
+              >
+                Description
+              </label>
+            )}
+            <input
+              type="text"
+              id="desc"
+              className="form-control"
+              value={description}
+              placeholder="New task description"
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+            <div>
+              {!editing ? (
+                <label htmlFor="prt" className="form-label">
+                  Priority:
+                  {' '}
+                  {priority}
+                </label>
+              ) : `priority: ${priority}`}
+              <input
+                type="range"
+                className="form-range"
+                min="0"
+                max="20"
+                step="1"
+                id="customRange3"
+                value={priority}
+                onChange={(e) => setPriority(parseInt(e.target.value, 10))}
+              />
+            </div>
+          </div>
+          {!editing && (
+            <label htmlFor="color" className="form-label">Color</label>
+          )}
           <select
             id="dropdown"
             className="form-control"
+            defaultValue={color}
             onChange={(e) => setColor(e.target.value)}
           >
             <option value="primary">Blue</option>
@@ -91,88 +126,20 @@ function TaskForm({
             <option value="danger">Red</option>
             <option value="warning">Yellow</option>
           </select>
-
-          <label htmlFor="prt" className="form-label">
-            Priority:
-            {' '}
-            {priority}
-          </label>
-          <input
-            type="range"
-            className="form-range"
-            min="0"
-            max="20"
-            step="1"
-            id="customRange3"
-            value={priority}
-            onChange={(e) => setPriority(parseInt(e.target.value, 10))}
-          />
-
-          <button className="btn btn-primary" type="submit">
-            Add
-          </button>
-        </form>
-      ) : (
-
-        <div className={`mx-5 card text-center ${task.done ? 'border-' : 'text-bg-'}${task.color}`}>
-          <form onSubmit={(e) => EditTask(e)}>
-            <div className="card-header px-5">
-              <input
-                type="text"
-                id="title"
-                className="form-control"
-                value={title}
-                placeholder="New task title"
-                onChange={(e) => setTitle(e.target.value)}
-                required
-              />
-            </div>
-            <div className="card-body">
-              <input
-                type="text"
-                id="desc"
-                className="form-control"
-                value={description}
-                placeholder="New task description"
-                onChange={(e) => setDescription(e.target.value)}
-                required
-              />
-              <div>
-                {`priority: ${priority}`}
-                <input
-                  type="range"
-                  className="form-range"
-                  min="0"
-                  max="20"
-                  step="1"
-                  id="customRange3"
-                  value={priority}
-                  onChange={(e) => setPriority(parseInt(e.target.value, 10))}
-                />
-              </div>
-            </div>
-            <select
-              id="dropdown"
-              className="form-control"
-              defaultValue={task.color}
-              onChange={(e) => setColor(e.target.value)}
-            >
-              <option value="primary">Blue</option>
-              <option value="secondary">Grey</option>
-              <option value="success">Green</option>
-              <option value="danger">Red</option>
-              <option value="warning">Yellow</option>
-            </select>
-            <div className="card-footer text-muted">
+          <div className="card-footer text-muted">
+            {editing ? (
               <span>
                 <button type="submit" className="btn btn-primary">Save</button>
                 <button type="button" className="btn btn-danger" onClick={() => CancelEdit()}>Cancel</button>
               </span>
-            </div>
-          </form>
-        </div>
-      )}
-
+            ) : (
+              <button className="btn btn-success" type="submit">
+                Add
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
